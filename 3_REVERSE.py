9b16e6fb49b98a4009d56beb58d4790adafd1999
Oihana1 <61787900+Oihana1@users.noreply.github.com>
2020-03-11 13:03:50 +0100

    Add files via upload

---
+++ 3_REVERSE.py
@@ -0,0 +1,12 @@
+#Oihana Arambarri
+# 1 tik 10 listako elementuak alderantziz imprimatu
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("List: ", list)
+list.reverse() #elementuak alderantziz jarri
+print('Alderantziz: ', list)
+
+
+
+
+
+
