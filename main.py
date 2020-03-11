b355fe6ea9d0dd564d96fdcbea86b696e8f2b51f
xImAlvaro <61787879+xImAlvaro@users.noreply.github.com>
2020-03-11 13:40:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+def menu():
+	#MENU DE OPCIONES
+	print("** ::::::::::::::::::::::::: **")
+	print(":: Aukeratu funtzio bat ::")
+	print("** ::::::::::::::::::::::::: **")
+	print("-------------------------------")
+	print("| Gehiketa:              ->  1     |")
+	print("| Kenketa:               ->  2     |")
+	print("| Biderketa:             ->  3     |")
+	print("| Zatiketa:              ->  4     |")
+	print("| Berreketa:             ->  5     |")
+	print("| Erroa:                 ->  6     |")
+	print("| Ezabatu:               ->  e     |")
+	print("| Irten:                 ->  i     |")
+	print("-------------------------------")
+	print("** ::::::::::::::::::::::::: **")
+	print("\n")
+
+def aukerak(auk=0):
+	aukera = int(input("Aukeratu funtzio bat "))
+	return aukera
+menu()
+auk=aukerak(auk=0)
+
+if(auk==1):
+    Gehiketa()
+elif(auk==2):
+    Kenketa()
+
+
+
